22d0c809918683dd56c132be8bab2edcd459b826
MarioGranada <mario.granada190@gmail.com>
2025-05-03 03:31:13 -0700

    feat: basic layout for header componenet

---
+++ app/about/page.tsx
@@ -0,0 +1,5 @@
+const About = async () => {
+  return <> about page</>;
+};
+
+export default About;
--- src/components/Layout/Header/index.tsx
+++ src/components/Layout/Header/index.tsx
@@ -1,9 +1,27 @@
+import Link from 'next/link';
+
 const Header = () => {
   return (
     <header>
-      Header signature leading to home about projects (list of projects with
-      more description and details) itbible Contact {'->'} shall be an
-      integration with calendly
+      <div className="flex justify-between">
+        <div>
+          <Link href="/">mario hernandez</Link>
+        </div>
+        <div className="flex gap-4">
+          <ul className="flex gap-4">
+            <li>
+              <Link href="about">about</Link>
+            </li>
+            <li>
+              <Link href="projects">projects</Link>
+            </li>
+            <li>
+              <Link href="it-bible">it bible</Link>
+            </li>
+          </ul>
+          <button>contact with calendly</button>
+        </div>
+      </div>
     </header>
   );
 };
