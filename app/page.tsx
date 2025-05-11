90d4d27f338967dba8c1598dc4dac8df5adc7484
MarioGranada <mario.granada190@gmail.com>
2025-05-11 10:26:38 -0700

    feat: adding home page content

--- app/page.tsx
+++ app/page.tsx
@@ -1,18 +1,43 @@
+import ProjectsList from '@/src/components/Pages/Home/ProjectsList';
+import Image from 'next/image';
+import Link from 'next/link';
+
 const Home = async () => {
   const projects = await fetch(
     'https://portfolio-db-43961-default-rtdb.firebaseio.com/projects.json'
   );
-  const data = await projects.json();
-  console.log('in here oe projects', data);
+  const { projects: projectsData } = await projects.json();
+  console.log('in here oe projects', projectsData);
   return (
-    <div>
-      Main profile image full rounded short description with read more link to
-      about Github contribution graph List of projects with image, title, short
-      description, link to project
-      {data.map((item: Record<string, string>) => (
-        <div key={item.name}> {item.name} </div>
-      ))}
-    </div>
+    <>
+      <section className="flex gap-4">
+        <div className="flex flex-col sm:flex-row gap-4">
+          <Image
+            src="/assets/images/portfolio_photo1.jpeg"
+            width={100}
+            height={100}
+            alt="Mario Hernandez Porfolio Photo"
+            className="clip-path-circle self-center"
+          />
+          <div>
+            <h1>Hi! Welcome to my site!</h1>
+            <p>
+              My name is Mario Hernandez. I am a software developer with a
+              passion for creating innovative solutions. I have a strong
+              background in software development and have worked on a wide range
+              of projects.
+            </p>
+            <Link href="/about">Want to read more?</Link>
+          </div>
+        </div>
+      </section>
+      <section className="flex justify-center w-full">
+        Github contribution graph
+      </section>
+      <section className="flex w-full">
+        <ProjectsList projects={projectsData} />
+      </section>
+    </>
   );
 };
 
