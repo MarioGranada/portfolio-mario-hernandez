59d0ce8a3cf099bcb3290880899c0385f0670692
MarioGranada <mario.granada190@gmail.com>
2025-06-20 10:22:35 -0700

    feat: add footer links

---
+++ src/components/Layout/Footer/IconLinks.ts
@@ -0,0 +1,31 @@
+import { LinkedIn, GitHub, Gmail, DailyDev, Spotify } from '../../BrandIcons';
+
+const IconLinks = [
+  {
+    href: 'https://www.linkedin.com/in/mariogranadacolombia/',
+    Icon: LinkedIn,
+    label: 'LinkedIn',
+  },
+  {
+    href: 'https://github.com/MarioGranada/',
+    Icon: GitHub,
+    label: 'GitHub',
+  },
+  {
+    href: 'mailto:mario.hernandez.dev@gmail.com',
+    Icon: Gmail,
+    label: 'Email me',
+  },
+  {
+    href: 'https://app.daily.dev/mariohernandez',
+    Icon: DailyDev,
+    label: 'Daily.dev',
+  },
+  {
+    href: 'https://open.spotify.com/user/12158189686?si=38af760c24354cde',
+    Icon: Spotify,
+    label: 'Spotify',
+  },
+];
+
+export default IconLinks;
